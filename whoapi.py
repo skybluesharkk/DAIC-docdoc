5fca43c22dc53429bc6a5ab6669324a2b8c3b16f
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-05-28 11:55:44 +0900

    fe

--- whoapi.py
+++
@@ -1,6 +0,0 @@
-import requests, pprint
-url = ("https://apps.who.int/gho/athena/api/GHO/TB_incidence"
-       "?filter=COUNTRY:IND;YEAR:2022;SEX:Both&format=json&profile=simple")
-r = requests.get(url, headers={"Accept": "application/json"})
-print(r.status_code, r.headers["Content-Type"])
-pprint.pp(r.json())
